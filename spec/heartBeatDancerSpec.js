afc5167afe2796820d257777543b12c85d688073
Breg Bromley <bregbromley@gmail.com>
2020-04-06 23:23:19 -0600

    added tests

--- spec/heartBeatDancerSpec.js
+++ spec/heartBeatDancerSpec.js
@@ -1,12 +1,26 @@
 describe('heartBeatDancer', function() {
 
-  it('should have a function ', function() {
-    expect().to.be.an.instanceof();
+  it('heartBeatDancer should exist', function() {
+    var heartBeatDancer = new HeartBeatDancer;
+    expect(typeof heartBeatDancer).to.equal('object');
   });
 
-  it('should ', function() {
 
-    expect().to.be.true;
+
+  it('heartBeatDancer.step should be a function', function() {
+    var heartBeatDancer = new HeartBeatDancer;
+    expect(typeof heartBeatDancer.step).to.equal('function');
   });
+
 
 });
+
+
+// it('should have a function ', function() {
+//   expect().to.be.an.instanceof();
+// });
+
+// it('should ', function() {
+
+//   expect().to.be.true;
+// });
